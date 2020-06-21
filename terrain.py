f02f94fea4163594a9c8489e92a45102df171685
Watto1337 <67204708+Watto1337@users.noreply.github.com>
2020-06-21 09:16:41 -0700

    Add files via upload

--- terrain.py
+++ terrain.py
@@ -1,14 +1,14 @@
 import noise
 
-def getTerrain(w, h, u, yScale):
+def getTerrain(w, h, u, yScale, buffer):
     hMap = noise.getNoise(w, h, u, yScale)
     terrain = []
-    for i in range(w * u):
-        for j in range(h * u):
-            terrain.append([[(i-w/2*u)*20,   hMap[i][j],     (j-h/2*u)*20],\
-                            [(i-1-w/2*u)*20, hMap[i-1][j],   (j-h/2*u)*20],\
-                            [(i-1-w/2*u)*20, hMap[i-1][j-1], (j-1-h/2*u)*20]])
-            terrain.append([[(i-w/2*u)*20,   hMap[i][j],     (j-h/2*u)*20],\
-                            [(i-w/2*u)*20,   hMap[i][j-1],   (j-1-h/2*u)*20],\
-                            [(i-1-w/2*u)*20, hMap[i-1][j-1], (j-1-h/2*u)*20]])
+    for i in range(-buffer, w * u + buffer):
+        for j in range(-buffer, h * u + buffer):
+            terrain.append([[(i-w/2*u)*15,  hMap[i%(w*u)][j%(h*u)],        (j-h/2*u)*15],\
+                            [(i-1-w/2*u)*15,hMap[(i-1)%(w*u)][j%(h*u)],    (j-h/2*u)*15],\
+                            [(i-1-w/2*u)*15,hMap[(i-1)%(w*u)][(j-1)%(h*u)],(j-1-h/2*u)*15]])
+            terrain.append([[(i-w/2*u)*15,  hMap[i%(w*u)][j%(h*u)],        (j-h/2*u)*15],\
+                            [(i-w/2*u)*15,  hMap[i%(w*u)][(j-1)%(h*u)],    (j-1-h/2*u)*15],\
+                            [(i-1-w/2*u)*15,hMap[(i-1)%(w*u)][(j-1)%(h*u)],(j-1-h/2*u)*15]])
     return [terrain, hMap]
